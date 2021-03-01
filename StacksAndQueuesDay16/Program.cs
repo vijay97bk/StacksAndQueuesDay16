22be5d4550544c4dc9226a1694a9d267e1013c4f
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-03-01 23:00:38 +0530

    [vijay]FirstCommit

---
+++ StacksAndQueuesDay16/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StacksAndQueuesDay16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to stacks and queue");
+        }
+    }
+}
